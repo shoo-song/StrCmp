8d1665259946f8faabd7a08d9005808979cb61b0
shoo.song <shoo.song@samsung.com>
2025-03-28 10:12:00 +0900

    lengh test

---
+++ Project7/StringCompare.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class StringCompare {
+public:
+	double CheckLength(string a, string b) {
+		double size_a = a.size();
+		double size_b = b.size();
+		double result = 0;
+		if (size_a == size_b) {
+			return 60;
+		}
+		double diff = 0;
+		if (size_a >= size_b) {
+			diff = size_a - size_b;
+			if (diff >= size_b) return 0;
+			return (1 - (diff / size_b)) * 60;
+		}
+		else {
+			diff = size_b - size_a;
+			if (diff >= size_a) return 0;
+			return (1 - (diff / size_a)) * 60;
+		}
+		return 0;
+	}
+	double compare(string a, string b) {
+		double result = 0;
+		result += CheckLength(a, b);
+		return result;
+	}
+};
--- Project7/StringCompare_test.cpp
+++ Project7/StringCompare_test.cpp
@@ -1,4 +1,14 @@
 #include "gmock/gmock.h"
+#include "StringCompare.cpp"
+TEST(StrCmp, TC1) {
+	EXPECT_EQ(StringCompare().CheckLength("ASD", "DSA"), 60);
+}
+TEST(StrCmp, TC2) {
+	EXPECT_EQ(StringCompare().CheckLength("A", "BB"), 0);
+}
+TEST(StrCmp, TC3) {
+	EXPECT_EQ(StringCompare().CheckLength("AA", "AAE"), 30);
+}
 int main(void) {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();

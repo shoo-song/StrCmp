6327874758a5a20c99ead8b31f120bda346a601b
shoo.song <shoo.song@samsung.com>
2025-03-28 11:08:08 +0900

    add alpha3

--- Project7/StringCompare_test.cpp
+++ Project7/StringCompare_test.cpp
@@ -1,13 +1,16 @@
 #include "gmock/gmock.h"
 #include "StringCompare.cpp"
 TEST(StrCmp, TC1) {
-	EXPECT_EQ(StringCompare().CheckLength("ASD", "DSA"), 100);
+	EXPECT_EQ(StringCompare().compare("ASD", "DSA"), 100);
 }
 TEST(StrCmp, TC2) {
-	EXPECT_EQ(StringCompare().CheckLength("A", "BB"), 0);
+	EXPECT_EQ(StringCompare().compare("A", "BB"), 0);
 }
 TEST(StrCmp, TC3) {
-	EXPECT_EQ(StringCompare().CheckLength("AA", "AAE"), 50);
+	EXPECT_EQ(StringCompare().compare("AA", "AAE"), 50);
+}
+TEST(StrCmp, TC4) {
+	EXPECT_EQ(StringCompare().compare("AAABB", "BA"), 40);
 }
 int main(void) {
 	::testing::InitGoogleMock();
